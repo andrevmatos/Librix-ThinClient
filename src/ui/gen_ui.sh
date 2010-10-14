54ab36f5babc3f620812239ed7c6a40cea3539ae
André Vitor <andrematos@las.ic.unicamp.br>
2010-10-14 12:26:23 -0300

    Deleting compile scripts.
    TODO: create new python compile scripts on init.py file

--- src/ui/gen_ui.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -x
-
-for i in *.ui; do
-    pyuic4 --from-imports -i 0 "$i" -o "$(echo $i | sed 's|.ui$|.py|g')"
-done
-exit 0
--- src/ui/icons/gen_rcc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -x
-
-pyrcc4 -py3 temp_icons.qrc -o ../temp_icons_rc.py
